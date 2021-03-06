15a8179b164db15be3282671a9d96564da55daa9
seunghun yang <yabby1997@gmail.com>
2021-03-06 13:31:22 +0900

    Modal 실습

---
+++ NavigationController/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  NavigationController
+//
+//  Created by Seunghun Yang on 2021/03/06.
+//
+
+import UIKit
+
+class ThirdViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
